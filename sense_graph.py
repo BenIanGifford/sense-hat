52ccfccb98a6369461496f76490571a834f76dc1
BenIanGifford <beniangifford@gmail.com>
2016-07-01 13:34:09 -0400

    Added a timestamp to sense_graph.py so you can tell the time when that reading was taken

--- sense_graph.py
+++ sense_graph.py
@@ -1,7 +1,8 @@
 from sense_hat import SenseHat
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 from time import sleep
-
+import datetime
 print("To exit and graph this data hit ctrl + c")
 
 sh = SenseHat()
@@ -9,15 +10,21 @@ temps = []
 times = []
 
 def graph():
-    times = [value**10 for value in range(len(temps))]
+    #times = [value for value in range(len(temps))]
+    plt.xticks(rotation=25)
     plt.ylabel("Tmperature", fontsize=14)
-    plt.xlabel("Time", fontsize=14)
+    plt.xlabel("Time")
+    ax = plt.gca()
+    xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+    ax.xaxis.set_major_formatter(xfmt)
     plt.plot(times, temps)
     plt.show()
     
 try:    
     while True:
         sleep(10)
+        now = datetime.datetime.now()
+        times.append(now)
         temp = sh.get_temperature()
         temps.append(temp)
 except KeyboardInterrupt:
